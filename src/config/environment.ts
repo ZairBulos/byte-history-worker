7076af2edbfa6270a6888ec481be407a8bd809ab
Zair Bulos <buloszair@gmail.com>
2025-07-09 13:40:27 -0300

    feat(worker): use local PostgreSQL URL in dev environment

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -3,16 +3,19 @@ import { env } from "node:process";
 import dotenv from "dotenv";
 dotenv.config();
 
-const ENV_KEYS = ["GOOGLE_GENERATIVE_AI_API_KEY", "DATABASE_URL"];
-
 export function loadEnv() {
+  const ENV_KEYS = ["NODE_ENV", "GOOGLE_GENERATIVE_AI_API_KEY", "DATABASE_URL"];
   for (const key of ENV_KEYS) {
     if (!env[key]) throw new Error(`Missing environment variable: ${key}`);
   }
 }
 
+const isDev = env.NODE_ENV === "development";
+
 const environment: Environment = {
-  DATABASE_URL: env.DATABASE_URL!,
+  DATABASE_URL: isDev
+    ? "postgresql://postgres:postgres@localhost:5432/postgres"
+    : env.DATABASE_URL!,
 };
 
 export default environment;
