c970027b843f6249a8dec0c3d8f3e10c35c66bab
Zair Bulos <buloszair@gmail.com>
2025-07-10 17:54:20 -0300

    feat(worker): simplify milestone saving using pool.query directly

--- src/db/save.ts
+++ src/db/save.ts
@@ -4,14 +4,8 @@ export async function saveMilestone(
   text: string,
   model: string
 ): Promise<void> {
-  const client = await pool.connect();
-
-  try {
-    await client.query(
-      "INSERT INTO tech_milestones (milestone, ai_model) VALUES ($1, $2)",
-      [text, model]
-    );
-  } finally {
-    client.release();
-  }
+  await pool.query(
+    "INSERT INTO tech_milestones (milestone, ai_model) VALUES ($1, $2)",
+    [text, model]
+  );
 }
